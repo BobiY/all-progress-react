c0ce498d7898e7467fcc5c8d2e85967f0adebf8c
yaoshuaibo@kanzhun.com <yaoshuaibo@kanzhun.com>
2023-02-16 21:26:00 +0800

    feat: 新增react 和 shared 基础包

---
+++ packages/react/index.ts
@@ -0,0 +1,12 @@
+import { jsx } from './src/jsx';
+/**
+ * File: index.ts
+ * Created Date: 2023-02-16 20:45:20
+ * Author: yao
+ * Last Modified: 2023-02-16 21:18:24
+ * describe：React 包导出放到
+ */
+export default {
+	version: '0.0.0',
+	createElement: jsx
+};
