93ef17ee79e921db54f7945c0d77ce23f6afe626
yaoshuaibo@kanzhun.com <yaoshuaibo@kanzhun.com>
2023-02-16 20:27:04 +0800

    x

---
+++ .commitlintrc.js
@@ -0,0 +1,12 @@
+/**
+ * File: .commitlintrc.js
+ * Created Date: 2023-02-16 20:22:13
+ * Author: yao
+ * Last Modified: 2023-02-16 20:22:49
+ * describe：
+ */
+
+
+module.exports = {
+    extends: ['@commitlint/config-conventional']
+}
---
+++ pacakges/index.ts
@@ -0,0 +1,9 @@
+/**
+ * File: index.ts
+ * Created Date: 2023-02-16 20:16:04
+ * Author: yao
+ * Last Modified: 2023-02-16 20:16:29
+ * describe：
+ */
+
+const a = 123;
